a8d4c048ee43264fba262274f107a387e0e4bfa9
TheUbMunster <theubmunster@gmail.com>
2025-01-19 18:31:26 -0700

    add comments to helpful resources

--- src/renderer.h
+++ src/renderer.h
@@ -1,5 +1,13 @@
 #pragma once
 
+//resources I found helpful when developing this:
+//https://www.codingwiththomas.com/blog/rendering-an-opengl-framebuffer-into-a-dear-imgui-window
+//https://github.com/ThoSe1990/opengl_imgui/blob/main/src/main.cpp
+//https://github.com/JoeyDeVries/LearnOpenGL/blob/master/src/4.advanced_opengl/5.2.framebuffers_exercise1/framebuffers_exercise1.cpp
+//https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples#example-for-opengl-users
+//chatgpt
+//https://learnopengl.com/Getting-started/Hello-Triangle
+
 #include "globalimguiglglfw.h"
 
 class Renderer 
