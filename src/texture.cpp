76195da3566236418949de724ce908cb36ad9b22
mateusfavarin <mateusfavarinc@gmail.com>
2025-04-05 23:11:50 -0300

    fix 8bpp

--- src/texture.cpp
+++ src/texture.cpp
@@ -49,7 +49,7 @@ int Texture::GetVRAMWidth() const
 	int ret = m_width;
 	Texture::BPP bpp = GetBPP();
 	if (bpp == Texture::BPP::BPP_4) { ret = static_cast<int>(std::ceil(static_cast<float>(m_width) / 4.0f)); }
-	else if (bpp == Texture::BPP::BPP_8) { ret = static_cast<int>(std::ceil(static_cast<float>(m_width) / 4.0f)); }
+	else if (bpp == Texture::BPP::BPP_8) { ret = static_cast<int>(std::ceil(static_cast<float>(m_width) / 2.0f)); }
 	return ret;
 }
 
