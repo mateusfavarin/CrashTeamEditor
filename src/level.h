712cde3a83e10cb3bfe2827ddb1ed69680c7279c
mateusfavarin <mateusfavarinc@gmail.com>
2025-09-10 10:30:22 -0700

    new default value

--- src/level.h
+++ src/level.h
@@ -71,7 +71,7 @@ private:
 	bool m_loaded;
 	bool m_genVisTree;
 	float m_maxLeafAxisLength = 64.0f;
-	float m_distanceFarClip = 300.0f;
+	float m_distanceFarClip = 1000.0f;
 	std::vector<std::tuple<std::string, std::string>> m_invalidQuadblocks;
 	std::string m_logMessage;
 	std::string m_name;
